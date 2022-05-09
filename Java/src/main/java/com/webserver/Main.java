a62f439cc092ece9c50372bc0f4d77bc6e44fd23
collins <collinsboniface1000@gmail.com>
2022-05-09 18:05:21 +0300

    Handle a new incoming message adding the debug message to read the accepted incoming message

--- Java/src/main/java/com/webserver/Main.java
+++ Java/src/main/java/com/webserver/Main.java
@@ -1,6 +1,7 @@
 package com.webserver;
 
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) throws Exception{
@@ -9,21 +10,25 @@ public class Main {
             System.out.println("Server started. \nListening for messages.");
 
             while (true) {
-            // Handle a new incoming message
+                // Handle a new incoming message
+                try(Socket client = serverSocket.accept()) {
+                    // Client <------- messages queued up in it!!
+                    System.out.println("Debug: got new message " + client.toString());
 
-            // Read the request - listen to the message
+                    // Read the request - listen to the message
 
-            // Decide how we'd like to respond
+                    // Decide how we'd like to respond
 
-            // Just send back a simple "Hello World"
+                    // Just send back a simple "Hello World"
 
-            // Send back an image?
+                    // Send back an image?
 
-            // Change response based on route?
+                    // Change response based on route?
 
-            // Send a response - send our reply
+                    // Send a response - send our reply
 
-            // Get ready for the next message
+                    // Get ready for the next message
+                }
             }
         }
     }
