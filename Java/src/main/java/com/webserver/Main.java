0a58575709e5233eda73a6b7e0a8afd88c46620a
collins <collinsboniface1000@gmail.com>
2022-05-09 17:43:04 +0300

    Giving the pros on what should be done and Created an instance of the ServerSocket class

--- Java/src/main/java/com/webserver/Main.java
+++ Java/src/main/java/com/webserver/Main.java
@@ -1,7 +1,27 @@
 package com.webserver;
 
+import java.net.ServerSocket;
+
 public class Main {
     public static void main(String[] args) throws Exception{
-        
+        // Start receiving messages - ready to receive messages!
+        try (ServerSocket serverSocket = new ServerSocket(8080)) {
+            // Handle a new incoming message
+
+            // Read the request - listen to the message
+
+            // Decide how we'd like to respond
+
+            // Just send back a simple "Hello World"
+
+            // Send back an image?
+
+            // Change response based on route?
+
+            // Send a response - send our reply
+
+            // Get ready for the next message
+
+        }
     }
 }
